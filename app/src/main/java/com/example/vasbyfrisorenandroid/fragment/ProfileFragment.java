fd32e3c052786750069153c2057b9045580a641e
Huntgangerino <axel1234516@hotmail.com>
2021-12-06 17:24:40 +0100

    fixed some things in profile

--- app/src/main/java/com/example/vasbyfrisorenandroid/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/vasbyfrisorenandroid/fragment/ProfileFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -17,22 +18,33 @@ import com.example.vasbyfrisorenandroid.model.service.Service;
 import com.example.vasbyfrisorenandroid.model.service.ServiceAdapter;
 import com.example.vasbyfrisorenandroid.model.setting.Setting;
 import com.example.vasbyfrisorenandroid.model.setting.SettingAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProfileFragment extends Fragment {
     private View rootView;
+
     private List<Setting> settingList;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private LinearLayoutManager layoutManager;
+
+    private FirebaseAuth auth;
+
+    private TextView profileName, profileEmail;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.profile, container, false);
+        auth = FirebaseAuth.getInstance();
+        profileName = rootView.findViewById(R.id.profile_name);
+        profileEmail = rootView.findViewById(R.id.profile_email);
+
         initRecyclerView();
         return rootView;
     }
@@ -40,6 +52,11 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+        FirebaseUser user = auth.getCurrentUser();
+        if(user != null){
+            profileName.setText(user.getDisplayName());
+            profileEmail.setText(user.getEmail());
+        }
     }
 
 
@@ -50,9 +67,20 @@ public class ProfileFragment extends Fragment {
     }
 
     private void initRecyclerView(){
+        creatingSettingList();
+
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(rootView.getContext());
+        adapter = new SettingAdapter(settingList);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void creatingSettingList(){
         settingList = new ArrayList<>();
-        settingList.add(new Setting(R.drawable.email_icon, "Email", "Loco"));
-        settingList.add(new Setting(R.drawable.contact2, "Telefon", "Loco"));
+        settingList.add(new Setting(R.drawable.email_icon, "Email", "Här kan du ändra ditt email"));
+        settingList.add(new Setting(R.drawable.contact2, "Telefon", "Här kan du ändra ditt telefonNr"));
         settingList.add(new Setting(R.drawable.email_icon, "Email", "Loco"));
         settingList.add(new Setting(R.drawable.email_icon, "Email", "Loco"));
         settingList.add(new Setting(R.drawable.email_icon, "Email", "Loco"));
@@ -61,12 +89,5 @@ public class ProfileFragment extends Fragment {
         settingList.add(new Setting(R.drawable.email_icon, "Email", "Loco"));
         settingList.add(new Setting(R.drawable.person_stroke, "Radera konto", "Loco"));
         settingList.add(new Setting(0, "Logga ut", null));
-
-        recyclerView = rootView.findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(rootView.getContext());
-        adapter = new SettingAdapter(settingList);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
     }
 }
